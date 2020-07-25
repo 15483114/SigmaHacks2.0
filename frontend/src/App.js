241e3d0d8bb5d219eef9d809369e2b8e9ea32550
15483114 <15483114.ik@usp.br>
2020-07-25 16:10:32 -0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Main from './pages/Main';
+import './global.css';
+
+function App() {
+  return (
+    <Main></Main>
+      
+   
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Main/Schedule/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+export default function Schedule(){
+return(
+    <div>
+        
+<h1>JOIN AN ORDER</h1>
+
+<button > START NEW ORDER </button>
+
+</div>
+    
+);
+
+}
