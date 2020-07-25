15b6efa2651503a85dcffe3c3de9b979866b9121
15483114 <15483114.ik@usp.br>
2020-07-25 17:37:16 -0300

    new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Main from './pages/Main';
+import Schedule from './pages/Schedule';
 import './global.css';
 
 function App() {
   return (
+    <div>
     <Main></Main>
-      
+    <Schedule></Schedule>  
+    </div>
+
    
   );
 }
--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -3,12 +3,12 @@ import './styles.css';
 
 export default function Main(){
 return(
-    <div>
-        
-<h1>JOIN AN ORDER</h1>
-
-<button > START NEW ORDER </button>
-
+    <div className= "mainDiv">
+        <button > START NEW ORDER </button>
+        <button > BECOME A PANDEMIC PATREON </button>
+        <div > <h1>JOIN AN ORDER</h1>
+        <button id="button2" > America Bar August, 5  5pm-7pm</button>
+        </div>    
 </div>
     
 );
